e2138f3b22641dba1598479f16a98fe627b195a7
Milliejayy <ifeanyisomtomillicent@gmail.com>
2025-07-27 11:21:19 +0100

    created add, edit and delete class

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
+from .managers import CustomUserManager
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class Author(models.Model):
@@ -45,11 +46,19 @@ class UserProfile(models.Model):
         ('Member', 'Member'),
     )
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(AbstractUser, on_delete=models.CASCADE)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
 
     def __str__(self):
         return f'{self.user.username} - {self.role}'
     
-
     
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    objects = CustomUserManager()  # Link to custom manager
+
+    def __str__(self):
+        return self.username
