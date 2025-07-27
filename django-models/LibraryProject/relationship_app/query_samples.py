9a6cda8652b7f2dfaa81cb9c6053335cc359cf7b
Milliejayy <ifeanyisomtomillicent@gmail.com>
2025-07-27 07:19:59 +0100

    list all books in library

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -7,6 +7,14 @@ django.setup()
 
 from relationship_app.models import Author, Book, Library, Librarian
 
+
+# 2. List all books in a library
+library = Library.objects.get(name="National Library")
+library_books = library.books.all()
+print(library_books)
+# for book in books_in_library:
+#     print(book.title)
+
 # 1. Query all books by a specific author
 author = Author.objects.get(name="Chinua Achebe")
 books_by_author = Book.objects.filter(author=author)
@@ -14,12 +22,6 @@ print("Books by Chinua Achebe:")
 for book in books_by_author:
     print(book.title)
 
-# 2. List all books in a library
-library = Library.objects.get(name="National Library")
-library.books.all()
-# print("\nBooks in National Library:")
-# for book in books_in_library:
-#     print(book.title)
 
 # 3. Retrieve the librarian for a library
 librarian = Librarian.objects.get(library=library)
