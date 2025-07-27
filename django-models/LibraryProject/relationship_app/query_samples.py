21039e45c827c88f15e987008506495462142ce9
Milliejayy <ifeanyisomtomillicent@gmail.com>
2025-07-27 10:37:58 +0100

    created add, edit and delete class

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,13 +1,21 @@
-from relationship_app.models import Author, Book, Library, Librarian
+from relationship_app.models import Author 
+from relationship_app.models import Book
+from relationship_app.models import Library
+from relationship_app.models import Librarian
 
 
 # 2. List all books in a library
-library = Library.objects.get(name="National Library")
-Library.objects.get(name="city_library")
-library_books = library.books.all()
-print(library_books)
-# for book in books_in_library:
-#     print(book.title)
+library_name = "Central Library"  # Or whatever name you're searching
+library = Library.objects.get(name=library_name)
+print(library)
+
+def get_library_by_name(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        print("Library found:", library)
+    except Library.DoesNotExist:
+        print("Library not found.")
+
 
 # 1. Query all books by a specific author
 author = Author.objects.get(name="Chinua Achebe")
